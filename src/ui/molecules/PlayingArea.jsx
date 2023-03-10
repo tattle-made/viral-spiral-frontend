e2f767cb2800cbb268d9cebd21c9ffc507bf8fa9
dennyabrain <denny.george90@gmail.com>
2023-03-10 09:54:19 +0530

    chore: disabled superpowers

--- src/ui/molecules/PlayingArea.jsx
+++ src/ui/molecules/PlayingArea.jsx
@@ -265,7 +265,7 @@ const PlayingArea = () => {
                             label={"Turn into fake news"}
                           ></Button>
                         ) : null} */}
-                        {gameStat.card.allowedActions.includes(
+                        {/* {gameStat.card.allowedActions.includes(
                           "mark_as_fake"
                         ) ? (
                           <Button
@@ -273,7 +273,7 @@ const PlayingArea = () => {
                             label={"Mark as fake"}
                             onClick={() => specialPowers("mark_as_fake")}
                           ></Button>
-                        ) : null}
+                        ) : null} */}
                         {gameStat.card.allowedActions.includes(
                           "encyclopedia_search"
                         ) ? (
@@ -293,7 +293,7 @@ const PlayingArea = () => {
                           ></Button>
                         ) : null} */}
 
-                        {gameStat.card.allowedActions.includes(
+                        {/* {gameStat.card.allowedActions.includes(
                           "viral_spiral"
                         ) ? (
                           <Button
@@ -303,7 +303,7 @@ const PlayingArea = () => {
                               specialPowers("viral_spiral_initiate")
                             }
                           ></Button>
-                        ) : null}
+                        ) : null} */}
                       </Box>
                     ) : null}
                   </Box>
